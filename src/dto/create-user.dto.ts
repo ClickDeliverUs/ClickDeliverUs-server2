59336d8996acfd94ced0e35566394635ad4db81e
Ozymandias089 <ozymandias089@gmail.com>
2023-04-03 14:59:38 +0900

    prettierrc 수정, createUserDto 추가, app.service, app.controller 수정

---
+++ src/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUserDto {
+  readonly email: string;
+  readonly userName: string;
+  readonly password: string;
+  readonly isAdult: boolean;
+  readonly tel: string;
+  readonly birth: string;
+  readonly addreess: string;
+}
