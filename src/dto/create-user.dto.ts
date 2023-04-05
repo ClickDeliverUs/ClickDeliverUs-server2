36f5d241462bcd3654da9ad664c18de8ad153fe2
Ozymandias089 <ozymandias089@gmail.com>
2023-04-05 17:06:32 +0900

    DTO 변경, auth/signUp 구현

--- src/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateUserDto {
-  readonly email: string;
-  readonly userName: string;
-  readonly password: string;
-  readonly isAdult: boolean;
-  readonly tel: string;
-  readonly birth: string;
-  readonly addreess: string;
-}
