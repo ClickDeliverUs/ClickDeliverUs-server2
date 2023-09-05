671bd93965f4bb59dbf9608210c40a3e92a7df44
Younghoon Choi <50650232+Ozymandias089@users.noreply.github.com>
2023-09-05 21:05:15 +0900

    Update products.module.ts
    파일 참조는 절대경로로

--- src/product/products.module.ts
+++ src/product/products.module.ts
@@ -3,21 +3,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Store } from './gscu/Store.entity';
-import { Alcohol } from './gscu/Alcohol.entity';
-import { Beverage } from './gscu/Beverage.entity';
-import { Candy } from './gscu/Candy.entity';
-import { Frozen } from './gscu/Frozen.entity';
-import { Ice } from './gscu/Ice.entity';
-import { Instant } from './gscu/Instant.entity';
-import { Lifeuse } from './gscu/Lifeuse.entity';
-import { Medic } from './gscu/Medic.entity';
-import { Milk } from './gscu/Milk.entity';
-import { Noodle } from './gscu/Noodle.entity';
-import { Onedate } from './gscu/Onedate.entity';
-import { Snack } from './gscu/Snack.entity';
-import { Tobacco } from './gscu/Tobacco.entity';
-import { Goods } from './gscu/goods';
+import { Store } from 'src/product/gscu/Store.entity';
+import { Alcohol } from 'src/product/gscu/Alcohol.entity';
+import { Beverage } from 'src/product/gscu/Beverage.entity';
+import { Candy } from 'src/product/gscu/Candy.entity';
+import { Frozen } from 'src/product/gscu/Frozen.entity';
+import { Ice } from 'src/product/gscu/Ice.entity';
+import { Instant } from 'src/product/gscu/Instant.entity';
+import { Lifeuse } from 'src/product/gscu/Lifeuse.entity';
+import { Medic } from 'src/product/gscu/Medic.entity';
+import { Milk } from 'src/product/gscu/Milk.entity';
+import { Noodle } from 'src/product/gscu/Noodle.entity';
+import { Onedate } from 'src/product/gscu/Onedate.entity';
+import { Snack } from 'src/product/gscu/Snack.entity';
+import { Tobacco } from 'src/product/gscu/Tobacco.entity';
+import { Goods } from 'src/product/gscu/goods
 
 @Module({
   imports: [
