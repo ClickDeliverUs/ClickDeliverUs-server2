76b7c70e5e45af8ecc61c97af332dc3505451884
Younghoon Choi <50650232+Ozymandias089@users.noreply.github.com>
2023-09-05 21:06:18 +0900

    Update products.module.ts
    파일참조 관련 노가다

--- src/product/products.module.ts
+++ src/product/products.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Store } from 'src/product/gscu/Store.entity';
 import { Alcohol } from 'src/product/gscu/Alcohol.entity';
 import { Beverage } from 'src/product/gscu/Beverage.entity';
 import { Candy } from 'src/product/gscu/Candy.entity';
@@ -17,7 +16,6 @@ import { Noodle } from 'src/product/gscu/Noodle.entity';
 import { Onedate } from 'src/product/gscu/Onedate.entity';
 import { Snack } from 'src/product/gscu/Snack.entity';
 import { Tobacco } from 'src/product/gscu/Tobacco.entity';
-import { Goods } from 'src/product/gscu/goods
 
 @Module({
   imports: [
