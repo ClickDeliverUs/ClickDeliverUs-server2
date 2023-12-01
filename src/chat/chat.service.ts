5a3eef386e8f6d6f436d32e79d419ddea46e1057
Ozymandias089 <ozymandias089@gmail.com>
2023-12-01 16:01:17 +0900

    added Exception handling to chatroom api

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class ChatService {
+  private logger: Logger = new Logger(ChatService.name);
   private chatRooms: Map<string, { expirationTime: Date }> = new Map();
   private chatMessages: Map<string, { messages: string[] }> = new Map();
 
@@ -11,6 +12,10 @@ export class ChatService {
     expirationTime.setHours(expirationTime.getHours() + 3);
 
     this.chatRooms.set(chatRoomId, { expirationTime });
+    this.logger.log(
+      `Chatroom Successfully created. chatRoomId: ${chatRoomId}, cid: ${cid}, did: ${did}`,
+    );
+    return { chatRoomId };
   }
   // TODO: 추후에 배달 기능 구현 후 해당 부분 수정 필요
   // 채팅방을 생성한 후, 해당 채팅방에 고객과 기사가 연결됨
@@ -29,12 +34,24 @@ export class ChatService {
 
   removeChatRoom(cid: string, did: string) {
     const chatRoomId = `${cid}-${did}`;
-    this.chatRooms.delete(chatRoomId);
+    try {
+      this.chatRooms.delete(chatRoomId);
+    } catch (err) {
+      this.logger.log(`Failed to destroy chatroom. ${err}`);
+      throw new BadRequestException(`Failed to destroy ChatRoom`);
+    }
+    this.logger.log(`ChatRoom destroyed`);
   }
   // TODO: 추후에 배달 기능 구현 후 해당 부분 수정 필요
   // 채팅방을 제거하는 시점은 배달 완료 후 expirationTime 이후임
 
   getAllChatRooms() {
-    return this.chatRooms;
+    try {
+      return this.chatRooms;
+    } catch (err) {
+      this.logger.log(`An error ocurred while getting chatrroms info`);
+      throw new BadRequestException(`An error ocurred while getting chatrroms info`);
+    }
+    this.logger.log(`Successfully fetched chatroom info`);
   }
 }
